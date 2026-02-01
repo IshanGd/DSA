3212e208152069c95b52d8646118d4c9c77cf644
Ishan Ghosh Dastider <ishanghoshdastider8a@gmail.com>
2026-02-01 11:06:47 +0530

    Time: 4 ms (99.64%) | Memory: 45.8 MB (83.02%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -3,7 +3,7 @@ class Solution {
         if (x < 0 || (x > 0 && x % 10 == 0)) {
             return false;
         }
-        int y = 0;
+        int y=0;
         for (; y < x; x /= 10) {
             y = y * 10 + x % 10;
         }
