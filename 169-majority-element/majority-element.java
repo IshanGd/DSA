a50082b361b54652ef4e1a90acd5469366d6642d
Ishan Ghosh Dastider <ishanghoshdastider8a@gmail.com>
2026-09-02 16:34:11 +0530

    Time: 2 ms (70.75%) | Memory: 55.8 MB (31.84%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,10 +1,14 @@
-import java.util.*;
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        if(nums.length%2==0)
-        return nums[nums.length/2];
-        else
-        return nums[(nums.length-1)/2];
+        int el=nums[0],ct=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==el)
+            ct++;
+            else
+            ct--;
+            if(ct==0)
+            el=nums[i+1];
+        }
+        return el;
     }
 }
