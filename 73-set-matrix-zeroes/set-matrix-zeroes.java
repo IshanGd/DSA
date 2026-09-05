c373a21ae1205a7c7ab2c42376675f0638d97531
Ishan Ghosh Dastider <ishanghoshdastider8a@gmail.com>
2026-09-06 00:09:14 +0530

    Time: 1 ms (96.95%) | Memory: 47.8 MB (11.14%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean fc=false,fr=false;
+        for(int i=0;i<matrix[0].length;i++){
+            if(matrix[0][i]==0)
+            fr=true;
+        }
+        for(int i=0;i<matrix.length;i++){
+            if(matrix[i][0]==0)
+            fc=true;
+        }
+        for(int i=1;i<matrix.length;i++){
+            for(int j=1;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        for(int i=1;i<matrix.length;i++){
+            for(int j=1;j<matrix[0].length;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(fc){
+            for(int i=0;i<matrix.length;i++){
+                matrix[i][0]=0;
+            }
+        }
+        if(fr){
+            for(int i=0;i<matrix[0].length;i++){
+                matrix[0][i]=0;
+            }
+        }
+    }
+}
